1a372ead81db998bf4002e5614b9e0e93c9dcab9
Rafhael Augusto <rafhaelalvesprado@gmail.com>
2025-06-19 18:53:53 -0300

    fix(deploy): ajustar 3 linhas de codigo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { ReactLenis } from "lenis/react";
 
 import { PrimaryButton } from "@/styles/GlobalStyles";
 import * as S from "./styles";
 
 export default function Home() {
-  const [state, setState] = useState({
-    isPageLoaded: false,
-  });
-
   useEffect(() => {
     window.scrollTo(0, 0);
-    setState((prev) => ({ ...prev, isPageLoaded: true }));
   }, []);
 
   return (
@@ -26,10 +21,7 @@ export default function Home() {
               <S.SecondaryText>UPCOMING LAUNCH</S.SecondaryText>
               <S.PrimaryText>AX-4 MISSION</S.PrimaryText>
             </div>
-            <PrimaryButton
-              pageloaded={state.isPageLoaded}
-              aria-label="Go to the spaceX website to learn more about an upcoming launch"
-            >
+            <PrimaryButton aria-label="Go to the spaceX website to learn more about an upcoming launch">
               <a
                 target="_blank"
                 href="https://www.spacex.com/launches/mission/?missionId=ax-4"
